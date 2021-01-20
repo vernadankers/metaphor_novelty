50e5bfb9287d1c0e2fce71fdae40cf6d3d59ddc4
vernadankers <vernadankers@gmail.com>
2021-01-20 10:44:15 +0000

    Updated evaluation function with trace writing

--- evaluate.py
+++ evaluate.py
@@ -2,10 +2,12 @@
 
 import logging
 import scipy.stats
+import torch
 from sklearn.metrics import precision_recall_fscore_support, mean_absolute_error
 
 
-def evaluate(model, dataloader, dataset_name="validation", output_filename=None):
+def evaluate(model, dataloader, dataset_name="validation", output_filename=None,
+             no_labels=False):
     """
     Evaluation metaphor model on VU Amsterdam metaphor test data.
 
@@ -26,17 +28,16 @@ def evaluate(model, dataloader, dataset_name="validation", output_filename=None)
             model(inputs, batch.mask)
 
         # Run model in inference mode
-        for i, (prediction, length) in enumerate(zip(metaphoricity_outputs, batch.lengths)):
-            prediction = prediction[:length].cpu().tolist()
-            prediction_m = transform(prediction, batch.mapping[i])
+        for i, (prediction_m, prediction_n, length) in enumerate(
+            zip(metaphoricity_outputs, novelty_outputs, batch.lengths)):
+            prediction_m = torch.round(prediction_m[:length - 1].cpu().squeeze(-1)).tolist()
+            prediction_m = transform(prediction_m, batch.mapping[i])
             target_m = batch.metaphoricity_labels[i]
-            pairs.extend([(t, round(p)) for t, p in \
+            pairs.extend([(t, p) for t, p in \
                 zip(target_m, prediction_m) if t != -2])
 
-        # Run model in inference mode
-        for i, (prediction, length) in enumerate(zip(novelty_outputs, batch.lengths)):
-            prediction = prediction[:length].cpu().tolist()
-            prediction_n = transform(prediction, batch.mapping[i])
+            prediction_n = prediction_n[:length - 1].cpu().tolist()
+            prediction_n = transform(prediction_n, batch.mapping[i])
             target_n = batch.novelty_labels[i]
             pairs_novelty.extend([(t, p) for t, p in \
                 zip(target_n, prediction_n) if t != -2])
@@ -59,14 +60,20 @@ def evaluate(model, dataloader, dataset_name="validation", output_filename=None)
     # If the output filename of the trace is specified, save the traces
     # to file
     if output_filename is not None:
-        with codecs.open(output_filename, 'w', encoding="utf-8") as f:
-            f.write(f"sentences\ttarget_metaphoricity\tprediction_metaphoricity\ttarget_novelty\tprediction_novelty\n")
+        with open(output_filename, 'w', encoding="utf-8") as f:
+            if no_labels:
+                f.write(f"sentences\tprediction_metaphoricity\tprediction_novelty\n")
+            else:
+                f.write(f"sentences\ttarget_metaphoricity\tprediction_metaphoricity\ttarget_novelty\tprediction_novelty\n")                
             for sentence, target_m, prediction_m, target_n, prediction_n in trace:
                 t_m = " ".join([str(x) for x in target_m])
                 p_m = " ".join([str(x) for x in prediction_m])
                 t_n = " ".join([str(round(x, 3)) for x in target_n])
                 p_n = " ".join([str(round(x, 3)) for x in prediction_n])
-                f.write(f"{' '.join(sentence)}\t{t_m}\t{p_m}\t{t_n}\t{p_n}\n")
+                if no_labels:
+                    f.write(f"{' '.join(sentence)}\t{p_m}\t{p_n}\n")
+                else:
+                    f.write(f"{' '.join(sentence)}\t{t_m}\t{p_m}\t{t_n}\t{p_n}\n")
     return f1, r
 
 
@@ -82,6 +89,6 @@ def transform(prediction_sent, ids_sent):
     """
     mapping = {i: None for i in range(max(ids_sent) + 1)}
     for prediction_word, index in zip(prediction_sent, ids_sent):
-        if mapping[index] is None or abs(mapping[index]) < abs(prediction_word):
+        if mapping[index] is None:
             mapping[index] = prediction_word
     return [mapping[i] for i in range(max(ids_sent) + 1)]
